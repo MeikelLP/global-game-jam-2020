327a6f265621e0eb875f802a786d472b25246379
Meikel Philipp <meikel.philipp@neos-it.de>
2020-02-01 11:57:22 +0100

    Improved UI Added scroll UI info

--- Assets/Scripts/CinemachineAdditionals.cs
+++ Assets/Scripts/CinemachineAdditionals.cs
@@ -5,38 +5,43 @@ using Cinemachine;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(CinemachineVirtualCamera), typeof(CinemachineFollowZoom))]
 public class CinemachineAdditionals : MonoBehaviour
 {
     [SerializeField] private float zoomSpeed = 0.1f;
-    [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private KeyCode key = KeyCode.Mouse1;
+    [SerializeField] private KeyCode rotateKey = KeyCode.Mouse1;
+    [SerializeField] private string zoomKey = "MouseScroll";
+
+    [SerializeField] private CinemachineFollowZoom zoom;
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private TextMeshProUGUI rotateText;
+    [SerializeField] private TextMeshProUGUI zoomText;
 
     private CinemachineOrbitalTransposer _transposer;
-    private CinemachineFollowZoom _zoom;
 
     private void Start()
     {
-        if (!text) throw new ArgumentNullException(nameof(text));
+        if (!rotateText) throw new ArgumentNullException(nameof(rotateText));
+        if (!zoomText) throw new ArgumentNullException(nameof(rotateText));
+        if (!zoom) throw new ArgumentNullException(nameof(rotateText));
 
-        var cam = GetComponent<CinemachineVirtualCamera>();
         _transposer = cam.GetCinemachineComponent<CinemachineOrbitalTransposer>();
-        _zoom = cam.GetComponent<CinemachineFollowZoom>();
+        zoom = cam.GetComponent<CinemachineFollowZoom>();
 
 
         _transposer.m_XAxis.m_InputAxisName = "";
         _transposer.m_XAxis.m_InputAxisValue = 0;
 
-        text.text = $"Rotate (<color=yellow>{key}</color>)";
+        rotateText.text = $"Rotate (<color=yellow>{rotateKey}</color>)";
+        zoomText.text = $"Zoom (<color=yellow>{zoomKey}</color>)";
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(rotateKey))
         {
             _transposer.m_XAxis.m_InputAxisName = "Mouse X";
         }
-        else if (Input.GetKeyUp(key))
+        else if (Input.GetKeyUp(rotateKey))
         {
             _transposer.m_XAxis.m_InputAxisName = "";
             _transposer.m_XAxis.m_InputAxisValue = 0;
@@ -45,8 +50,8 @@ public class CinemachineAdditionals : MonoBehaviour
         var scroll = Input.mouseScrollDelta;
         if (Math.Abs(scroll.y) > 0)
         {
-            _zoom.m_Width -= scroll.y * zoomSpeed;
-            _zoom.m_Width = Mathf.Clamp(_zoom.m_Width, 0, 1);
+            zoom.m_Width -= scroll.y * zoomSpeed;
+            zoom.m_Width = Mathf.Clamp(zoom.m_Width, 0, 1);
         }
     }
 }
