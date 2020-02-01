01e05527b572d6b047b052bae09863a85d8d65be
Meikel Philipp <meikel.philipp@neos-it.de>
2020-02-01 11:49:27 +0100

    Added info text for rotate

--- Assets/Scripts/CinemachineAdditionals.cs
+++ Assets/Scripts/CinemachineAdditionals.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(CinemachineVirtualCamera), typeof(CinemachineFollowZoom))]
 public class CinemachineAdditionals : MonoBehaviour
 {
     [SerializeField] private float zoomSpeed = 0.1f;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private KeyCode key = KeyCode.Mouse1;
 
     private CinemachineOrbitalTransposer _transposer;
     private CinemachineFollowZoom _zoom;
 
     private void Start()
     {
+        if (!text) throw new ArgumentNullException(nameof(text));
+
         var cam = GetComponent<CinemachineVirtualCamera>();
         _transposer = cam.GetCinemachineComponent<CinemachineOrbitalTransposer>();
         _zoom = cam.GetComponent<CinemachineFollowZoom>();
@@ -21,15 +26,17 @@ public class CinemachineAdditionals : MonoBehaviour
 
         _transposer.m_XAxis.m_InputAxisName = "";
         _transposer.m_XAxis.m_InputAxisValue = 0;
+
+        text.text = $"Rotate (<color=yellow>{key}</color>)";
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetKeyDown(key))
         {
             _transposer.m_XAxis.m_InputAxisName = "Mouse X";
         }
-        else if (Input.GetMouseButtonUp(1))
+        else if (Input.GetKeyUp(key))
         {
             _transposer.m_XAxis.m_InputAxisName = "";
             _transposer.m_XAxis.m_InputAxisValue = 0;
@@ -38,7 +45,7 @@ public class CinemachineAdditionals : MonoBehaviour
         var scroll = Input.mouseScrollDelta;
         if (Math.Abs(scroll.y) > 0)
         {
-            _zoom.m_Width += scroll.y * zoomSpeed;
+            _zoom.m_Width -= scroll.y * zoomSpeed;
             _zoom.m_Width = Mathf.Clamp(_zoom.m_Width, 0, 1);
         }
     }
