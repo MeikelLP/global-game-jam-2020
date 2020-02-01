0e1e4169fd9f895501166ac2d9eee193612574f1
_ChrisP_ <HdJdh62xZurb5gtXM5IB>
2020-02-01 21:20:18 +0100

    bla

---
+++ Assets/Scripts/ItemShop/BankAccountBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankAccountBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _money;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public bool Debit(float value)
+    {
+        float temp = _money;
+        _money -= value;
+        if (_money < 0)
+        {
+            _money = temp;
+            return false;
+        }
+
+        return true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ItemShop/ItemShopBehaviour.cs
+++ Assets/Scripts/ItemShop/ItemShopBehaviour.cs
@@ -3,44 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class ItemShopBehaviour : MonoBehaviour
 {
-     private Text _keyInfoText;
-    
-        public KeyCode _open;
-    
-        private GameObject _scrollView;
+    private Text _keyInfoText;
 
-        private List<Image> _images;
-        
-        // Start is called before the first frame update
-        void Start()
+    public KeyCode _open;
+
+    public GameObject contentPage;
+
+    private List<Image> _images;
+
+    public ManagerBehaviour _manager;
+
+    public GameObject _button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _keyInfoText = GetComponentInChildren<Text>();
+        _keyInfoText.text = "[" + _open.ToString() + "] Shop";
+        contentPage.SetActive(false);
+        FillShop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_open))
         {
-            _keyInfoText = GetComponentInChildren<Text>();
-            _keyInfoText.text = "[" + _open.ToString() + "] Shop";
-            _scrollView = GameObject.Find("Shop");
-            _scrollView.SetActive(false);
-            ScanForComponents();
-        }
-    
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(_open) )
+            if (contentPage.activeSelf)
             {
-                if (_scrollView.activeSelf)
-                {
-                    _scrollView.SetActive(false);
-                }
-                else
-                {
-                    _scrollView.SetActive(true);
-                }
+                contentPage.SetActive(false);
+            }
+            else
+            {
+                contentPage.SetActive(true);
             }
         }
-    
-        private void ScanForComponents()
+    }
+
+    private void FillShop()
+    {
+        foreach (var phonePart in _manager.phoneComponentDictionary)
         {
-            
+            GameObject newButton = Instantiate(_button,Vector3.zero,Quaternion.identity,contentPage.transform);
+            newButton.GetComponent<Button>().onClick.AddListener((() => _manager.BuyComponent(phonePart.Key)));
+            newButton.transform.SetParent(contentPage.transform);
         }
+    }
 }
