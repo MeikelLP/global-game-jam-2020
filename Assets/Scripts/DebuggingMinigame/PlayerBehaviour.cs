aa0569a6083e8b926ffeb7aaed1502f4b100ac5e
_ChrisP_ <HdJdh62xZurb5gtXM5IB>
2020-02-01 10:51:33 +0100

    bla

--- Assets/Scripts/DebuggingMinigame/PlayerBehaviour.cs
+++ Assets/Scripts/DebuggingMinigame/PlayerBehaviour.cs
@@ -11,40 +11,68 @@ public class PlayerBehaviour : MonoBehaviour
 
     private Boolean _isPlayerInBounds;
 
+    private Rigidbody2D _rigid;
+
+    public float speed = 0.1f;
+
+    public Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerTransform = gameObject.transform;
         _screen = GameObject.Find("Background");
         _isPlayerInBounds = true;
+        _rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdatePosition();
+        Debug.Log(_playerTransform.position);
     }
 
     private void UpdatePosition()
     {
-        if (_isPlayerInBounds)
-        {
-            Vector3 currentPos = _playerTransform.position;
-            if (Input.GetAxis("Vertical") > 0)
-            {
-                _playerTransform.position = currentPos + Vector3.up * 0.1f;
-            }
-            else if(Input.GetAxis("Horizontal") < 0)
-            {
-                _playerTransform.position = currentPos + Vector3.left * 0.1f;
-            }else if (Input.GetAxis("Vertical") < 0)
-            {
-                _playerTransform.position = currentPos + Vector3.down * 0.1f;
-            }else if (Input.GetAxis("Horizontal") > 0)
-            {
-                _playerTransform.position = currentPos + Vector3.right * 0.1f;
-            }   
+        Vector3 currentPos = _playerTransform.position;
+        if (Input.GetAxis("Vertical") > 0)
+        { 
+            TryMove(currentPos,Vector3.up * speed);
         }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            TryMove(currentPos,Vector3.left * speed);
+        }else if (Input.GetAxis("Vertical") < 0)
+        {
+            TryMove(currentPos,Vector3.down * speed);
+        }else if (Input.GetAxis("Horizontal") > 0)
+        {
+            TryMove(currentPos,Vector3.right * speed);
+        }
+
+       //Vector3 lowerLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+       //Vector3 upperRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+    }
+
+    private void TryMove(Vector3 oldPos, Vector3 add)
+    {
+        Vector3 targetPos = oldPos + add;
+        if (IsPlayerInBoundsTest(targetPos))
+        {
+            _playerTransform.position = targetPos;
+        }
+        else
+        {
+            _playerTransform.position = oldPos;
+        }
+    }
+
+    private bool IsPlayerInBoundsTest(Vector3 targetPos)
+    {
+        
+        return targetPos.x < mainCamera.scaledPixelWidth &&
+               targetPos.y < mainCamera.scaledPixelHeight;
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -53,7 +81,6 @@ public class PlayerBehaviour : MonoBehaviour
         {
             _isPlayerInBounds = false;
         }
-        Debug.Log("Exit!");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -62,6 +89,5 @@ public class PlayerBehaviour : MonoBehaviour
         {
             _isPlayerInBounds = true;
         }
-        Debug.Log("Enter!");
     }
 }
