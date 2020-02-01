00164039aeee2058c765aa651a419410708226d9
Meikel Philipp <meikel.philipp@neos-it.de>
2020-02-01 11:35:28 +0100

    Updated Oneplus phone Added mouse0 to select info Added screen material

--- Assets/Scripts/Assembler.cs
+++ Assets/Scripts/Assembler.cs
@@ -1,21 +1,28 @@
 using System;
 using System.Security.Cryptography;
 using DefaultNamespace;
+using TMPro;
 using UnityEngine;
 
 public class Assembler : MonoBehaviour
 {
     [SerializeField] private InventoryScript inventory;
     [SerializeField] private new Camera camera;
+    [SerializeField] private KeyCode key = KeyCode.Mouse0;
+    [SerializeField] private TextMeshProUGUI text;
 
     private void Start()
     {
         if(!camera) throw new ArgumentNullException(nameof(camera));
+        if(!inventory) throw new ArgumentNullException(nameof(inventory));
+        if(!text) throw new ArgumentNullException(nameof(text));
+
+        text.text = $"Select (<color=yellow>{key}</color>)";
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(key))
         {
             if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out var hit))
             {
