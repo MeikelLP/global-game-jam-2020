868ff4d755be46a0d8b56f6061169311768acc65
Meikel Philipp <meikel.philipp@neos-it.de>
2020-02-01 12:56:20 +0100

    Added hover svg

--- Assets/Scripts/Assembler.cs
+++ Assets/Scripts/Assembler.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using DefaultNamespace;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Assembler : MonoBehaviour
 {
@@ -10,34 +11,54 @@ public class Assembler : MonoBehaviour
     [SerializeField] private new Camera camera;
     [SerializeField] private KeyCode key = KeyCode.Mouse0;
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Color assembleColor = Color.green;
+    [SerializeField] private Color disassembleColor = Color.red;
+    [SerializeField] private SVGImage icon;
+
 
     private void Start()
     {
-        if(!camera) throw new ArgumentNullException(nameof(camera));
-        if(!inventory) throw new ArgumentNullException(nameof(inventory));
-        if(!text) throw new ArgumentNullException(nameof(text));
+        if (!camera) throw new ArgumentNullException(nameof(camera));
+        if (!inventory) throw new ArgumentNullException(nameof(inventory));
+        if (!text) throw new ArgumentNullException(nameof(text));
 
         text.text = $"Select (<color=yellow>{key}</color>)";
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        if (Input.GetKeyDown(key))
+        icon.gameObject.SetActive(false);
+        Cursor.visible = true;
+        if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out var hit))
         {
-            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out var hit))
+            if (hit.collider.gameObject.TryGetComponent<PhonePart>(out var part))
             {
-                if (hit.collider.gameObject.TryGetComponent<PhonePart>(out var part))
+                var toolMode = PhoneSelection.Instance.toolMode;
+                var color = toolMode == ToolMode.Assemble ? assembleColor : disassembleColor;
+
+                icon.gameObject.SetActive(true);
+                icon.color = color;
+                icon.rectTransform.position = camera.WorldToScreenPoint(hit.point);
+                Cursor.visible = false;
+
+                if (Input.GetKeyDown(key))
                 {
-                    if (PhoneSelection.Instance.toolMode == ToolMode.Assemble)
+                    if (toolMode == ToolMode.Assemble)
                     {
                         inventory.Remove(part);
                     }
-                    else if(PhoneSelection.Instance.toolMode == ToolMode.Disassemble)
+                    else if (toolMode == ToolMode.Disassemble)
                     {
                         inventory.Add(part);
                     }
                 }
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        icon.gameObject.SetActive(false);
+        Cursor.visible = true;
     }
 }
